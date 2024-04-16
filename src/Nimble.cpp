ae9c22043aef9f2a22d7ac179c31687beab052fc
jmacgowan <macgowan.joseph@gmail.com>
2024-04-16 15:00:46 +0100

    Added ninitial tokenization of return and numbers

--- src/Nimble.cpp
+++ src/Nimble.cpp
@@ -1,5 +1,57 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
+#include <optional>
+#include <vector>
+
+enum class TokenType {
+    _return,
+    _int_lit,
+    semi
+};
+
+struct Token
+{
+    TokenType type;
+    std::optional<std::string> value;
+};
+
+std::vector<Token> tokenize(const std::string& str){
+    std::vector<Token> tokens;
+    std::string buf;
+    for (int i = 0; i < str.length(); i++)
+    {
+        char c = str.at(i);
+        if (std::isalpha(c))
+        {
+            buf.push_back(c);
+            i++;
+            while (isalnum(str.at(i))){
+                buf.push_back(str.at(i));
+                i++;
+            }
+            i--;
+
+            if (buf == "return"){
+                tokens.push_back({.type = TokenType::_return});
+                buf.clear();
+                continue;
+            } else if(std::isdigit(c)){
+                while (isalnum(str.at(i))){
+                    buf.push_back(str.at(i));
+                    i++;
+                    }  
+            i--;
+            tokens.push_back({.type = TokenType::_int_lit, .value = buf});
+            buf.clear();
+            } else if (std::isspace(c)){
+                continue;
+            }
+            
+        }
+        
+    }
+}
 
 int main(int argc, char const *argv[])
 {
@@ -8,5 +60,15 @@ int main(int argc, char const *argv[])
         std::cerr << "nim <input.ni>" << std::endl;
         return EXIT_FAILURE;
     }
+
+    std::string contents;
+    {
+        std::stringstream contents_stream;
+        std::fstream input(argv[1], std::ios::in);
+        contents_stream << input.rdbuf();
+        contents = contents_stream.str();
+    }
+
+    tokenize(contents);
     return 0;
 }
